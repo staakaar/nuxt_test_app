2c4620616e3f8fe2512e09b9a249362a1e39e0cf
staakaar <vancyroletaka@gmail.com>
2023-11-27 00:00:44 +0900

    fix data table lyaout

--- components/organisms/Main.vue
+++ components/organisms/Main.vue
@@ -1,10 +1,96 @@
 <template>
-    <VDataTable :items="items" class="mt-12"></VDataTable>
+    <VSheet
+        class="mt-12"
+        height="700"
+        width="inherit"
+        rounded
+        border
+    >
+    <h1>test</h1>
+    <VBtn>ボタン</VBtn>
+        <VDataTable
+            :items="items"
+            class="mt-12"
+            height="600"
+        ></VDataTable>
+    </VSheet>
 </template>
 
 <script setup lang="ts">
 
 const items = [
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    },
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    },
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    },
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    },
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    },
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    }
+    ,
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    }
+    ,
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    }
+    ,
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    }
+    ,
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    }
+    ,
+    {
+        id: 1,
+        name: "test",
+        age: 23,
+        postCode: "201-4817"
+    }
+    ,
     {
         id: 1,
         name: "test",
