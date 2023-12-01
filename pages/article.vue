5fb173e8d4cc009e12d414c9439f26ea44602c27
staakaar <vancyroletaka@gmail.com>
2023-12-01 23:57:20 +0900

    wip defined drawer

--- pages/article.vue
+++ pages/article.vue
@@ -1,9 +1,31 @@
 <template>
-    <Main class="ml-10" />
+    <Main class="ml-10" @open-detail-drawer="openDetailDrawer" />
+    <!-- ドロワー -->
+    <VNavigationDrawer
+        :rail="rail"
+        rail-width="700"
+        v-model="drawer"
+        location="right"
+        scrim="gray"
+        absolute
+        temporary
+    />
 </template>
 
 <script setup lang="ts">
 import Main from '~/components/organisms/Main.vue';
+
+const drawer = ref(false);
+const rail = ref(false);
+
+/** drawer data object */
+
+/** drawer open */
+function openDetailDrawer(item: any) {
+    console.log(item)
+    console.log("こっち")
+}
+
 </script>
 
 <style scoped>
