72442b1c79a58c02a0c72f027a8504c05e924133
staakaar <vancyroletaka@gmail.com>
2023-12-02 23:59:29 +0900

    add settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(vuetify({ autoImport: true }))
+      })
+    },
+    '@nuxtjs/tailwindcss'
+  ],
+  css: ['~/assets/css/main.scss'],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  components:[
+    {
+      path: '~/components/',
+      pathPrefix: false,
+    }
+  ]
 })
